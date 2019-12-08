a5bc987dbbc955fd7b19ee1a5201a81bc9ce799c
maliut <lqn619@163.com>
2019-12-08 18:48:58 +0800

    first commit

---
+++ debug/GenerateTestUserSig.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+function genTestUserSig(userID) {
+ 
+  var SDKAPPID = 1400294749;
+  var EXPIRETIME = 604800;
+  var SECRETKEY = '29e433950484389c3050ede42055dce934c0a335a87494495008794d740b8e48';
+  var generator = new window.LibGenerateTestUserSig(SDKAPPID, SECRETKEY, EXPIRETIME);
+  var userSig = generator.genTestUserSig(userID);
+  return {
+    SDKAppID: SDKAPPID,
+    userSig: userSig
+  };
+}
